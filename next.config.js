bd53778836558173c542ff4c2c19f05443933491
Cody Lin <clin@ibm.com>
2020-08-01 12:01:04 -0700

    decrease size of images

--- next.config.js
+++ next.config.js
@@ -2,9 +2,9 @@ const withPlugins = require("next-compose-plugins");
 const withSass = require("@zeit/next-sass");
 const webpack = require("webpack");
 const path = require("path");
-const withOptimizedImages = require("next-images");
+const withImages = require("next-images");
 
-module.exports = withPlugins([[withSass], [withOptimizedImages]], {
+module.exports = withPlugins([[withSass], [withImages]], {
 	webpack(config, options) {
 		config.resolve.modules.push(path.resolve("./"));
 		return config;
